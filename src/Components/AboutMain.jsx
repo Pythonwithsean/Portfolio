d5c449a4ff742b8ec6393702cccf260849ad0c7f
Sean <107402787+Pythonwithsean@users.noreply.github.com>
2023-10-13 21:17:02 +0100

    "Commit"

--- src/Components/AboutMain.jsx
+++ src/Components/AboutMain.jsx
@@ -1,16 +1,127 @@
-import "../Styles/About.css";
+/* eslint-disable react/no-unescaped-entities */
 
 function AboutMain() {
   return (
     <div>
-      <p>
-        I'm a self-taught software developer with experience in JavaScript,
-        Python, HTML, and CSS. I'm highly motivated and detail-oriented,
-        knowledgeable about creating dynamic user interfaces, writing
-        maintainable code, and developing responsive web and mobile
+      <p className=" text-white p-5 text-sm text-justify">
+        I'm a <strong>self-taught</strong> software developer with experience in
+        JavaScript, Python, HTML, and CSS. I'm highly motivated and
+        detail-oriented, knowledgeable about creating dynamic user interfaces,
+        writing maintainable code, and developing responsive web and mobile
         applications. I have strong analytical and problem-solving abilities and
         work well both individually and in a team.
       </p>
+      <div className=" text-center flex flex-col">
+        <h2 className="font-bold w-full  mt-1 mb-0 ml-auto mr-auto text-center text-lg text-white">
+          Tech Stack
+        </h2>
+        <br />
+        <div className="w-full flex align-middle justify-center gap-3 p-1">
+          <img
+            align="left"
+            alt="Java"
+            width="50px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"
+          />
+          <img
+            align="left"
+            alt="TypeScript"
+            width="50px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg"
+          />
+          <img
+            align="left"
+            alt="Git"
+            width="50px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+          />
+          <img
+            align="left"
+            alt="Linux"
+            width="50px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg"
+          />
+          <img
+            align="left"
+            alt="HTML"
+            width="50px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg"
+          />
+          <img
+            align="left"
+            alt="CSS"
+            width="50px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg"
+          />
+          <img
+            align="left"
+            alt="JavaScript"
+            width="50px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
+          />
+          <img
+            align="left"
+            alt="React"
+            width="50px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+          />
+          <img
+            align="left"
+            alt="NodeJS"
+            width="50px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+          />
+          <img
+            align="left"
+            alt="Python"
+            width="50px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-plain.svg"
+          />
+          <img
+            align="left"
+            alt="GitHub"
+            width="50px"
+            src="https://raw.githubusercontent.com/Pythonwithsean/Pythonwithsean/887a2d7450eafee021dd42c9f31139c240455619/github.svg"
+          />
+          <img
+            align="left"
+            alt="Bash"
+            width="30px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg"
+          />
+          <img
+            align="left"
+            alt="Svelte"
+            width="30px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg"
+          />
+          <img
+            align="left"
+            alt="Django"
+            width="30px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg"
+          />
+          <img
+            align="left"
+            alt="Adobe XD"
+            width="30px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/xd/xd-plain.svg"
+          />
+          <img
+            align="left"
+            alt="Tailwind CSS"
+            width="30px"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+          />
+
+          <br />
+        </div>
+        <div className="">
+          <h2 className="font-bold w-full  mt-9 mb-0 ml-auto mr-auto text-center text-lg text-white">
+            My services
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,13 +1,18 @@
 import Header from "../Components/Header";
 import AboutMain from "../Components/AboutMain";
 import "../Styles/About.css";
+import "../Styles/index.css";
 
 function About() {
   return (
     <>
       <Header />
-      <h2>About Me</h2>
-      <AboutMain />
+      <div className="flex flex-col  w-1200 ml-auto mr-auto mt-6 ">
+        <h2 className=" font-bold w-full  mt-5 mb-0 ml-auto mr-auto text-center text-lg text-white">
+          About Me
+        </h2>
+        <AboutMain />
+      </div>
     </>
   );
 }
