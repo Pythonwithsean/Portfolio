6ed701ab9667a8fbd8db7b88ea1359faf599c98c
Sean <107402787+Pythonwithsean@users.noreply.github.com>
2023-10-13 20:09:50 +0100

    "Commit"

--- src/Components/AboutMain.jsx
+++ src/Components/AboutMain.jsx
@@ -1,7 +1,18 @@
 import "../Styles/About.css";
 
 function AboutMain() {
-  return <div>hell</div>;
+  return (
+    <div>
+      <p>
+        I'm a self-taught software developer with experience in JavaScript,
+        Python, HTML, and CSS. I'm highly motivated and detail-oriented,
+        knowledgeable about creating dynamic user interfaces, writing
+        maintainable code, and developing responsive web and mobile
+        applications. I have strong analytical and problem-solving abilities and
+        work well both individually and in a team.
+      </p>
+    </div>
+  );
 }
 
 export default AboutMain;
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,12 @@
 import Header from "../Components/Header";
 import AboutMain from "../Components/AboutMain";
+import "../Styles/About.css";
 
 function About() {
   return (
     <>
       <Header />
+      <h2>About Me</h2>
       <AboutMain />
     </>
   );
