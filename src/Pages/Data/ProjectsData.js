fa3985777334562a901b39951c32c80c78e06c8f
Pythonwithsean <Lunafcs@Protonmail.com>
2024-02-15 12:19:26 +0000

    "Commit"

--- src/Pages/Data/ProjectsData.js
+++ src/Pages/Data/ProjectsData.js
@@ -1,33 +1,56 @@
 const projectsData = [
   {
     id: 1,
+    name: "Quartzx Space | Sean Idisi",
+    language:
+      "ReactJS, TailwindCSS, TypeScript, ExpressJS, MongoDB, Quill, Sockets.io",
+    link: "https://github.com/Pythonwithsean/Quartzx-Space",
+    image: "../QuartzxSpaceNew.png",
+  },
+  {
+    id: 2,
     name: "Hallmark Healthcare Services | hhcs.uk",
     language: "ReactJS, NodeJS, ExpressJS, MongoDB",
     link: "https://hhcs.uk/",
     image: "../Capture.PNG",
   },
   {
-    id: 2,
+    id: 3,
     name: "Codershub Uk | codershub.uk",
     language: "HTML, CSS, JavaScript",
     link: "https://codershub.uk/",
     image: "../Coderhubuk.png",
   },
   {
-    id: 3,
+    id: 4,
     name: "Portfolio | Sean Idisi",
     language: "ReactJS, TailwindCSS, TypeScript",
     link: "https://pythonwithsean.netlify.app/",
     image: "../Sean.png",
   },
+
   {
-    id: 4,
-    name: "Quartzx Space | Sean Idisi",
-    language: "ReactJS, TailwindCSS, TypeScript, ExpressJS, MongoDB",
-    link: "https://github.com/Pythonwithsean/Quartzx-Space",
-    image: "../Quartzx.PNG",
+    id: 5,
+    name: "ToDo-React-App-Ts",
+    language: "ReactJS, TypeScript, Redis",
+    link: "https://github.com/Pythonwithsean/ToDo-React-App-Ts",
+    image: "../TodoApp.PNG",
   },
-  // Add more projects here as needed
+  {
+    id: 6,
+    name: "Loooffyy - Anime Streaming",
+    language: "ReactJS, NodeJS, ExpressJS, MongoDB, Graphql, jQuery",
+    link: "https://github.com/Pythonwithsean/Loooffyy-Anime-Streaming-Website",
+    image: "../Loooffyy.png",
+  },
+  {
+    id: 7,
+    name: "BellaBeauty | Mums Website",
+    language:
+      "ReactJS, Postgresql, Express, Typescript, Docker, HTML,CSS, Stripe",
+    link: "https://github.com/Pythonwithsean/Loooffyy-Anime-Streaming-Website",
+    image: "../Bellabeauty.png",
+  },
 ];
 
 export default projectsData;
