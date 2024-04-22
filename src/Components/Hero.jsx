577cf44a60de517938c9eb0b3213c23b91dcc71c
Sean <107402787+Pythonwithsean@users.noreply.github.com>
2024-04-22 07:57:47 +0100

    "Commit"

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-
 function Hero() {
   return (
     <>
@@ -14,7 +13,7 @@ function Hero() {
             <h3>I am a Back-End Developer</h3>
             <a href="https://github.com/Pythonwithsean">
               <br />
-              <img className="moving-text" src="https://readme-typing-svg.demolab.com/?lines=Full-stack%20web%20and%20app%20developer;Experienced%20UI%2FUX%20Designer;1%20year%20of%20coding%20experience;Always%20learning%20new%20things&font=Fira%20Code&center=true&width=1300px&height=66&color=4ed9ac&vCenter=true&pause=1000&size=25" alt="Moving Text" />
+              <img className="moving-text" src="https://readme-typing-svg.demolab.com/?lines=Full-stack%20web%20and%20app%20developer;Experienced%20UI%2FUX%20Designer;1%20year%20of%20coding%20experience;Always%20learning%20new%20things&font=Fira%20Code&center=true&width=1300px&height=100&color=4ed9ac&vCenter=true&pause=1000&size=25" alt="Moving Text" />
             </a>
             <Link to="/About">
               <button>About Me</button>
