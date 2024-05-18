3f6238a1ce2f628f4e799b1cb2a07387261e6648
Pythonwithsean <Pythonwithsean@gmail.com>
2024-05-18 21:55:24 +0100

    "Commmit"

--- src/Pages/Data/ProjectsData.js
+++ src/Pages/Data/ProjectsData.js
@@ -49,7 +49,7 @@ const projectsData = [
     name: "Golang Image to Ascii Cli App",
     language: "Golang",
     link: "https://github.com/Pythonwithsean/Atoi-Golang-Image-to-Ascii-Cli-App",
-    image: "https://private-user-images.githubusercontent.com/107402787/330593983-0122dbcd-6a4a-40c4-a56f-b8c9f5c972c9.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTYwNjQ5MjIsIm5iZiI6MTcxNjA2NDYyMiwicGF0aCI6Ii8xMDc0MDI3ODcvMzMwNTkzOTgzLTAxMjJkYmNkLTZhNGEtNDBjNC1hNTZmLWI4YzlmNWM5NzJjOS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNTE4JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDUxOFQyMDM3MDJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT05MzdmMTM0NDgyOTNkODc0MjE2OTkxOGJkNzdhYTM4YzlmYzFhZmE5NDk0YjNkZmMxOTY5NDIyMzRiZDAyNzEyJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.-SYaeiLVenQqPJ0hEzUTv9aBsjKrfFjucYDkn6rNJnQ"
+    image: "https://raw.githubusercontent.com/dawsonbooth/ascii-art/master/logo.png"
   
   },
   {
