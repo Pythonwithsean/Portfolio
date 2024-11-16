c6062deaa575c040a75da36835e267a97a35802c
Pythonwithsean <pythonwithsean@gmail.com>
2024-11-16 21:14:20 +0000

    Add blog and liffe

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,7 @@
-import { lazy, Suspense } from "react";
 import "../Styles/Home.css";
-import { Mail, Linkedin, Github } from "lucide-react";
+import { Mail, Linkedin, Github, Youtube } from "lucide-react";
 
 export default function HomePage() {
-  const Languages = lazy(() => import("../Components/Languages"));
   return (
     <div>
       <div className="main">
@@ -34,16 +32,26 @@ export default function HomePage() {
         </section>
 
         <section className="About">
-          <h2>Technical Languages</h2>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Languages />
-          </Suspense>
-          <br />
           <h2>About Me</h2>
           <p>
             A second-year scholar and self-taught software engineer with strong
             skills in data structures, algorithms, and building interfaces and
             backend services. Passionate about Golang, Tech.
+          </p>
+          <br />
+          <h2>Technical Languages</h2>
+          <p>
+            <strong>Programming Languages:</strong> Go, Python, JavaScript,
+            TypeScript, Java, C++, SQL, HTML, CSS
+          </p>
+          <p>
+            <strong>Frameworks:</strong> React, Express, Flask, Django, Spring
+            Boot, Svelte, Gin, Echo, GraphQL & Apollo, Next.js, Redux, Jest,
+            Cypress
+          </p>
+          <p>
+            <strong>Tools:</strong> Docker, Git, Postman, VS Code, Jira, Figma,
+            AWS S3, Firebase, AWS Lambda,
           </p>
         </section>
         <section className="Work-Experience">
@@ -52,20 +60,85 @@ export default function HomePage() {
           <p>
             {" "}
             Working on the backend services and building interfaces for the
-            company&apos;s product.
+            company&apos;s Internal Tooling.
           </p>
         </section>
         <section className="Projects">
           <h2>Projects</h2>
-          <h3>Image to Ascii Generator</h3>
-          <p>Languages - Go</p>
+          <h3>Image to ASCII Generator</h3>
+          <p>
+            A fun and creative tool built with Go that converts images into
+            ASCII art. This project demonstrates my ability to work with image
+            processing libraries and handle data manipulation in Go. It includes
+            support for customizable output dimensions, grayscale shading, and
+            file export options for seamless sharing.
+          </p>
+          <p>Languages & Tools: Go</p>
 
-          <h3>Rest API in Go</h3>
-          <p>Languages - Go, Docker</p>
+          <h3>REST API in Go</h3>
+          <p>
+            A robust and scalable REST API designed using Go and containerized
+            with Docker. This project highlights my expertise in building
+            efficient backends, including implementing CRUD operations,
+            middleware, and database integrations. With Docker, the API is
+            easily portable and ready for deployment across various
+            environments.
+          </p>
+          <p>Languages & Tools: Go, Docker</p>
+        </section>
+        <section className="Education">
+          <h2>Education</h2>
+          <h3>
+            Computer Science @ University of Kent, Canterbury | 2023 - 2027
+          </h3>
+          <p>
+            Currently pursuing a Bachelor of Science in Computer Science with a
+            focus on software engineering and data structures. I have completed
+            courses in algorithms, programming languages, and system design,
+            furthering my knowledge and skills in the field.
+          </p>
+        </section>
+        <section className="LifeAndBlog">
+          <h1>Life & Blog</h1>
+          <section>
+            <h2>Placed 3rd in Google Hackathon</h2>
+            <img src="/images/Googlehackathon.png" alt="Google"></img>
+            <p>
+              I participated in the Google Hackathon and secured the 3rd place
+              with my team. We built a web application that helps users learn
+              any African language of their choice. Using Google Cloud
+              Speech-to-Text API, we were able to transcribe and translate
+              spoken words into the selected language. The project was
+              well-received by the judges and attendees, and we were awarded a
+              cash prize for our efforts.
+            </p>
+          </section>
+          <section>
+            <h2>My Journey to Software Engineering</h2>
+            <p>
+              I started my journey to software engineering in high school when I
+              developed a keen interest in technology and programming. I began
+              learning Python and building small projects to enhance my skills.
+              As I progressed, I explored other programming languages and
+              frameworks, expanding my knowledge and expertise in the field. I
+              am now pursuing a degree in Computer Science to further my
+              education and career in software engineering.
+            </p>
+
+            <h2>My Love for Chess</h2>
+            <p>
+              Chess has been a significant part of my life since childhood. I
+              started playing chess at a young age and quickly fell in love with
+              the game. I have participated in various tournaments and
+              competitions, honing my skills and strategic thinking. Chess has
+              taught me patience, discipline, and critical thinking, which I
+              apply to my daily life and work as a software engineer.
+            </p>
+          </section>
         </section>
       </div>
       <footer>
-        <h2>Contact</h2>
+        <h2>Connect with Me</h2>
         <div className="contact-container">
           <a href="mailto:pythonwithsean@gmail.com">
             <Mail />
@@ -75,6 +148,9 @@ export default function HomePage() {
           </a>
           <a href="https://github.com/Pythonwithsean">
             <Github />
+          </a>
+          <a href="https://youtube.com/@pythonwithsean">
+            <Youtube />
           </a>
         </div>
         <div>
